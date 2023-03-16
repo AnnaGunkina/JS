3d737f009d9d326ba1630abce7f55a4dae85c837
Anna Gunkina <gunkinaaa@mail.ru>
2023-03-17 01:04:17 +0300

    Добавили DZ11

---
+++ Shop/data.js
@@ -0,0 +1,61 @@
+"use strict";
+
+function getProductsList() {
+  return JSON.parse(json).goods;
+}
+
+const json = `{
+  "goods": [
+      {
+        "id": "1",
+        "img": "/Shop/img/product1.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+     },
+     {
+        "id": "2",
+        "img": "/Shop/img/product2.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+     },
+     {
+        "id": "3",
+        "img": "/Shop/img/product3.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+     },
+     {
+        "id": "4",
+        "img": "/Shop/img/product4.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+     },
+     {
+        "id": "5",
+        "img": "/Shop/img/product5.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+     },
+     {
+        "id": "6",
+        "img": "/Shop/img/product6.jpg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+      }
+   ]
+}`;
+ 
+
+
+
+
+
+
+
+
